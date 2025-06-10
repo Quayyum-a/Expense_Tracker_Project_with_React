898b5c118b0c70aa4def7be1530b4c1ace70a08b
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 16:09:17 +0100

    added functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import './ExpensesFilter'
+import ExpenseFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses =(props) => {
