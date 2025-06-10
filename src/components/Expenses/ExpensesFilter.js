5e6f50cfdd457f4eed3ad78b7f5428cd5ecaea05
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 15:55:04 +0100

    added functionality

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -2,4 +2,23 @@ import React from 'react';
 
 import './ExpensesFilter.css'
 
-const ExpenseFilter = ()
+const ExpenseFilter = (props) => {
+  const dropDownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+  return (
+    <div className = 'expenses-filter'>
+      <div className = 'expenses-filter_control'>
+        <label>Filter by Year</label>
+        <select value = {props.selected} onChange={dropDownChangeHandler}>
+          <option value = '2025'>2025</option>
+          <option value = '2024'>2024</option>
+          <option value = '2023'>2023</option>
+          <option value = '2022'>2022</option>
+        </select>
+
+      </div>
+    </div>
+
+  );
+};
