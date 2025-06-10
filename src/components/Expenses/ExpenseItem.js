44993e867a1def136d6e868fb5225a985eb6f5d2
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 15:55:46 +0100

    added functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,9 +5,7 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("updated!!!");
-  };
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
