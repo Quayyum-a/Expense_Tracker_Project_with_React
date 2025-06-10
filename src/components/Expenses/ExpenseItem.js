b9483ef312cea8997d765479360e2104a5b1aa19
Quayyum-a <quayyumariyo@gmail.com>
2025-06-10 15:56:32 +0100

    added functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,8 +3,6 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-
-
   
   return (
     <Card className="expense-item">
